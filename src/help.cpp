280e2b1c5acdb07fb926aac64d04456073d10933
Marek Grzyb <grzybdev@gmail.com>
2019-12-02 18:19:50 +0100

    Finished printVersion function

--- src/help.cpp
+++ src/help.cpp
@@ -1,1 +1,7 @@
 #include "help.h"
+
+void printVersion() {
+    cout << "Brainfuck " << APP_VERSION << " by Marek Grzyb (GrzybDev)" << endl;
+    cout << "This project is licensed under the terms of the MIT license" << endl;
+    cout << "https://github.com/GrzybDev/Brainfuck" << endl << endl;
+}
