de7de5178c398eead374ed4e2e14d8fd0eb02812
Marek Grzyb <grzybdev@gmail.com>
2019-12-02 17:50:12 +0100

    Added short and long opts constants in main header file

--- src/main.h
+++ src/main.h
@@ -1,4 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <getopt.h>
+
+const static char* short_opts = "hvi:";
+const static option long_opts[] = {
+    {"help", no_argument, nullptr, 'h'},
+    {"version", no_argument, nullptr, 'v'},
+    {"input", required_argument, nullptr, 'i'},
+    {nullptr, no_argument, nullptr, 0}
+};
+
 #endif // MAIN_H
