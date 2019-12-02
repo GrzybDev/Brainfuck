4ff8e36afec78128ccc0e69c09a8e3141fb12c12
Marek Grzyb <grzybdev@gmail.com>
2019-12-02 18:20:09 +0100

    Finished printHelp function

--- src/help.cpp
+++ src/help.cpp
@@ -5,3 +5,10 @@ void printVersion() {
     cout << "This project is licensed under the terms of the MIT license" << endl;
     cout << "https://github.com/GrzybDev/Brainfuck" << endl << endl;
 }
+
+void printHelp() {
+    cout << "Usage: Brainfuck [-h|-v][-i <input file>]" << endl << endl;
+    cout << "-h | --help - Displays this message" << endl;
+    cout << "-v | --version - Displays application version" << endl << endl;
+    cout << "-i <file path> | --input <file path> - Executes program located at file path" << endl << endl;
+}
